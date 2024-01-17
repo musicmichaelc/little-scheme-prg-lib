667f3f35e55931cf468f52d447dfff04f1a18575
Michael Christensen <michaeldev@musicmichaelc.com>
2024-01-17 13:45:29 +0100

    Initial commit
    Starting off the project of creating several small programs / libraries
    in scheme for learning purposes...

---
+++ math.scm
@@ -0,0 +1,13 @@
+(define (mixed-fraction r)
+  "Converts ratio `r` to a mixed fraction -- returns a pair with an integer (in the `car`) and the leftover fraction (in the `cdr`)"
+  (cons (floor r) (- r (floor r))))
+
+(define (hr-ratio r)
+  "Gives a human readable form of the ratio `r` -- that is, converts it to a mixed fraction and returns it in string form"
+  (let ((cfr (mixed-fraction r)))
+    (if (zero? (cdr cfr))
+        (number->string (car cfr))
+        (string-append 
+          (number->string (car cfr)) 
+          " " 
+          (number->string (cdr cfr))))))
---
+++ temperature.scm
@@ -0,0 +1,15 @@
+(define (f-to-c f)                                                                   
+  "Converts Fahrenheit `f` to Celsius. Returns number as a fraction if possible (unless it is an integer)."
+  (* (- f 32) 5/9))
+
+(define (c-to-f c)                                                                   
+	"Converts Celsius `c` to Fahrenheit. Returns number as a fraction if possible (unless it is an integer)."
+  (+ 32 (* c 9/5)))
+
+(define (fc f)
+  "Converts Fahrenheit `f` to Celsius. Returns result as a human readable string, i.e. as a mixed fraction if it is not a whole number."
+	(hr-ratio (f-to-c (exact f))))
+
+(define (cf c)
+  "Converts Celsius `c` to Fahrenheit. Returns result as a human readable string (i.e. as a mixed fraction."
+	(hr-ratio (c-to-f (exact c))))
